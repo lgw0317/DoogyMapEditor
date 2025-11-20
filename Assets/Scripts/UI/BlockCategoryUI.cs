645dc83630a8cbb7856ce07ed09ec67d27a796a7
JangWh <janghy95@naver.com>
2025-11-20 16:57:09 +0900

    Revert "."
    This reverts commit 2ede52f03ed5da47a5bc5799e93893cd96f32c02.

--- Assets/Scripts/UI/BlockCategoryUI.cs
+++ Assets/Scripts/UI/BlockCategoryUI.cs
@@ -12,13 +12,12 @@ public class BlockCategoryUI : MonoBehaviour
     [Header("카테고리별 블록 목록")]
     public List<BlockListData> allCategories; // 블록, 동물, 기믹 등
 
-    //public List<BlockListData> blockData; // 1번 카테고리 (예: 블록)
-    //public List<BlockListData> animalData; // 2번 카테고리 (예: 동물)
-    //public List<BlockListData> gimickData; // 3번 카테고리 (예: 기믹)
-
     private List<GameObject> spawnedCategoryButtons = new List<GameObject>();
     private List<GameObject> spawnedButtons = new List<GameObject>();
     private List<BlockData> currentList; // 현재 표시 중인 블록 리스트
+
+    private int currentPage = 0;
+    private const int pageSize = 9;
 
     void Start()
     {
@@ -28,44 +27,46 @@ public class BlockCategoryUI : MonoBehaviour
     void Update()
     {
         // 카테고리 전환
-        //if (Input.GetKeyDown(KeyCode.F1))
-        //    ShowBlocks(blockData);
-        //if (Input.GetKeyDown(KeyCode.F2))
-        //    ShowBlocks(animalData);
-        //if (Input.GetKeyDown(KeyCode.F3))
-        //    ShowBlocks(gimickData);
-        //if (Input.GetKeyDown(KeyCode.Escape))
-        //{
-        //    ClearBlocks();
-        //    EditorManager.Instance.EditorMode = EditorMode.Idle;
-        //}
-
         // 단축키 블록 선택 (1~9)
         HandleCategoryHotkeys();
         HandleBlockHotkeys();
+        HandlePageChange();
     }
 
     public void ShowBlocks(BlockListData category)
     {
-        ClearBlocks();
         currentList = new List<BlockData>(category.blocks);
+        currentPage = 0;
 
-        for (int i = 0; i < currentList.Count; i++)
+        RefreshPage();
+    }
+
+    private void RefreshPage()
+    {
+        ClearBlocks();
+
+        if (currentList == null) return;
+
+        int start = currentPage * pageSize;
+        int end = Mathf.Min(start + pageSize, currentList.Count);
+
+        for (int i = start; i < end; i++)
         {
             var data = currentList[i];
+
             var go = Instantiate(blockButtonPrefab, blockListParent);
             go.GetComponent<Image>().sprite = data.icon;
             go.GetComponent<Button>().onClick.AddListener(() => EditorManager.Instance.ChooseBlock(data));
             spawnedButtons.Add(go);
 
-            // 숫자 표시 (TextMeshPro)
+            // 숫자표시
             var keyText = go.transform.Find("KeyText")?.GetComponent<TextMeshProUGUI>();
             if (keyText != null)
-                keyText.text = (i + 1).ToString();
-        EditorManager.Instance.EditorMode = EditorMode.Place;
+                keyText.text = ((i - start) + 1).ToString();
         }
-    }
 
+        EditorManager.Instance.EditorMode = EditorMode.Place;
+    }
     public void ClearBlocks()
     {
         foreach (var btn in spawnedButtons)
@@ -85,10 +86,10 @@ public class BlockCategoryUI : MonoBehaviour
             var text = go.GetComponentInChildren<TextMeshProUGUI>();
             text.text = category.categoryName;
 
-            //button.onClick.AddListener(() => ShowBlocks(category));
             spawnedCategoryButtons.Add(go);
         }
     }
+
     private void HandleCategoryHotkeys()
     {
         if (allCategories == null || allCategories.Count == 0) return;
@@ -100,19 +101,6 @@ public class BlockCategoryUI : MonoBehaviour
                 ShowBlocks(allCategories[i]);
             }
         }
-        //if (Input.GetKeyDown(KeyCode.F1))
-        //    ShowBlocks(allCategories[0]);
-
-        //if (Input.GetKeyDown(KeyCode.F2))
-        //    ShowBlocks(allCategories[1]);
-
-        //if (Input.GetKeyDown(KeyCode.F3))
-        //    ShowBlocks(allCategories[2]);
-
-        //if (Input.GetKeyDown(KeyCode.F4))
-        //    ShowBlocks(allCategories[3]);
-        //Todo : 테마 추가(allCategories에 추가 ScirptableObject가 생길시 여기에 코드 추가
-        //예시
 
         if (Input.GetKeyDown(KeyCode.Escape))
         {
@@ -120,20 +108,41 @@ public class BlockCategoryUI : MonoBehaviour
             EditorManager.Instance.EditorMode = EditorMode.Idle;
         }
     }
+
     private void HandleBlockHotkeys()
     {
         if (currentList == null || currentList.Count == 0) return;
 
         //에디터매니저의 모드가 배치모드가 아닐 경우 동작 안하도록 처리
         if (EditorManager.Instance.EditorMode != EditorMode.Place) return;
-        for (int i = 0; i < currentList.Count && i < 9; i++)
+
+        int start = currentPage * pageSize;
+        int end = Mathf.Min(start + pageSize, currentList.Count);
+
+        for (int i = 0; i < end - start; i++)
         {
             // 숫자키 1~9 확인
             KeyCode key = KeyCode.Alpha1 + i;
             if (Input.GetKeyDown(key))
             {
-                EditorManager.Instance.ChooseBlock(currentList[i]);
+                EditorManager.Instance.ChooseBlock(currentList[i + (currentPage * 9)]);
             }
+        }
+    }
+
+    private void HandlePageChange()
+    {
+        if (currentList == null || currentList.Count <= pageSize) return;
+
+        if (Input.GetKeyDown(KeyCode.Tab))
+        {
+            int maxPage = (currentList.Count - 1) / pageSize;
+            currentPage++;
+
+            if (currentPage > maxPage)
+                currentPage = 0; // 다시 처음 페이지로
+
+            RefreshPage();
         }
     }
 }
